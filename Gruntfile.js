fd02ba047011b7c45209ce90793097512cf8efb3
James <james@jamesandrewwright.co.uk>
2015-09-09 18:58:15 +0100

    added grunt-release

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
     "use strict";
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-release');
     require("load-grunt-tasks")(grunt);
 
 
